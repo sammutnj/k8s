6ae96060e05893b42476fc33a8f5a1c7f9c0f7ab
sammutnj <160558597+sammutnj@users.noreply.github.com>
2025-03-23 17:37:11 +1100

    Updated

--- terraform/main.tf
+++ terraform/main.tf
@@ -21,7 +21,7 @@ resource "aws_iam_role" "eks_role" {
 
 resource "aws_eks_cluster" "k8s_cluster" {
   name     = "my-k8s-cluster"
-  role_arn = aws_iam_role.eks_role.arn
+  role_arn = aws_iam_role.existing_role.arn
 
   vpc_config {
     subnet_ids = ["subnet-077c56108854be58b", "subnet-0750c0ee6baff8f23"]
