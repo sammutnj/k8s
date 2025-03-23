d95106b7af8a4d06e11b91cfb3461990b9d5a09f
sammutnj <160558597+sammutnj@users.noreply.github.com>
2025-03-23 20:48:27 +1100

    removed provider

--- terraform/worker-nodes.tf
+++ terraform/worker-nodes.tf
@@ -1,7 +1,3 @@
-provider "aws" {
-  region = "ap-southeast-2"
-}
-
 resource "aws_eks_node_group" "worker_nodes" {
   cluster_name  = aws_eks_cluster.k8s_cluster.name
   node_role_arn = "arn:aws:iam::843960079237:role/GHA-CICD"
