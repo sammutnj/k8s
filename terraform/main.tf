14c697cbff8668a97d1b42a30439e52bcc076fd6
sammutnj <sammutnj90@gmail.com>
2025-03-31 22:46:17 +1100

    Updated ebs driver

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,10 +13,49 @@ provider "aws" {
 
 resource "aws_eks_cluster" "k8s_cluster" {
   name     = "my-k8s-cluster"
-  role_arn = "arn:aws:iam::843960079237:role/GHA-CICD"  # Directly reference your IAM role ARN
+  role_arn = "arn:aws:iam::843960079237:role/GHA-CICD"
 
   vpc_config {
     subnet_ids = ["subnet-077c56108854be58b", "subnet-0750c0ee6baff8f23"]
   }
 
+}
+
+#  Kubernetes Provider (Needed for Helm)
+provider "kubernetes" {
+  host                   = aws_eks_cluster.k8s_cluster.endpoint
+  cluster_ca_certificate = base64decode(aws_eks_cluster.k8s_cluster.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+}
+
+#  Helm Provider (To deploy Helm charts)
+provider "helm" {
+  kubernetes {
+    host                   = aws_eks_cluster.k8s_cluster.endpoint
+    cluster_ca_certificate = base64decode(aws_eks_cluster.k8s_cluster.certificate_authority[0].data)
+    token                  = data.aws_eks_cluster_auth.cluster.token
+  }
+}
+
+#  Deploy AWS EBS CSI Driver via Helm
+resource "helm_release" "aws_ebs_csi_driver" {
+  name       = "aws-ebs-csi-driver"
+  repository = "https://kubernetes-sigs.github.io/aws-ebs-csi-driver"
+  chart      = "aws-ebs-csi-driver"
+  namespace  = "kube-system"
+
+  set {
+    name  = "controller.serviceAccount.create"
+    value = "true"
+  }
+
+  set {
+    name  = "controller.serviceAccount.name"
+    value = "ebs-csi-controller-sa"
+  }
+
+  set {
+    name  = "controller.serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
+    value = "arn:aws:iam::843960079237:role/GHA-CICD"
+  }
 }
