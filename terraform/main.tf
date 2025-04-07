2c48e40bde3f9915af0686273cfa002698b98ef7
sammutnj <sammutnj90@gmail.com>
2025-04-07 22:40:13 +1000

    Updated IAM role to use my own

--- terraform/main.tf
+++ terraform/main.tf
@@ -21,12 +21,51 @@ resource "aws_eks_cluster" "k8s_cluster" {
   }
 }
 
-# ✅ EKS Authentication Data Source
+data "aws_eks_cluster" "this" {
+  name = aws_eks_cluster.k8s_cluster.name
+}
+
+resource "aws_iam_openid_connect_provider" "eks" {
+  url             = data.aws_eks_cluster.this.identity[0].oidc[0].issuer
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = ["9e99a48a9960b14926bb7f3b02e22da0ecd2a84e"] # AWS OIDC thumbprint
+}
+
+data "aws_iam_policy_document" "ebs_assume_role" {
+  statement {
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+    principals {
+      type        = "Federated"
+      identifiers = [aws_iam_openid_connect_provider.eks.arn]
+    }
+    condition {
+      test     = "StringEquals"
+      variable = "${replace(data.aws_eks_cluster.this.identity[0].oidc[0].issuer, "https://", "")}:sub"
+      values   = ["system:serviceaccount:kube-system:ebs-csi-controller-sa"]
+    }
+  }
+}
+
+data "aws_iam_role" "ebs_csi_driver" {
+  name = "GHA-EBSCSIDRIVER"
+}
+
+resource "aws_iam_role" "ebs_csi_driver" {
+  name               = "AmazonEKS_EBS_CSI_DriverRole"
+  assume_role_policy = data.aws_iam_policy_document.ebs_assume_role.json
+}
+
+resource "aws_iam_role_policy_attachment" "ebs_csi_driver_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+  role       = data.aws_iam_role.ebs_csi_driver.name
+}
+
+# EKS Authentication Data Source
 data "aws_eks_cluster_auth" "cluster" {
   name = aws_eks_cluster.k8s_cluster.name
 }
 
-# ✅ Fix: Just reference EKS attributes in provider (No depends_on needed)
+# EKS attributes in provider (No depends_on needed)
 provider "kubernetes" {
   host                   = aws_eks_cluster.k8s_cluster.endpoint
   cluster_ca_certificate = base64decode(aws_eks_cluster.k8s_cluster.certificate_authority[0].data)
@@ -59,7 +98,8 @@ resource "helm_release" "aws_ebs_csi_driver" {
   }
 
   set {
-    name  = "controller.serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
-    value = "arn:aws:iam::843960079237:role/GHA-CICD"
+  name  = "controller.serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
+  value = data.aws_iam_role.ebs_csi_driver.arn
   }
+
 }
