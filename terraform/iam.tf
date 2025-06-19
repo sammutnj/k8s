40eecfb80b668f60da5c0cf8ed060bb155e81734
sammutnj <sammutnj90@gmail.com>
2025-06-19 23:51:19 +1000

    dd

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -19,14 +19,3 @@ resource "aws_iam_role" "alb_ingress_controller" {
     ]
   })
 }
-
-resource "kubernetes_service_account" "alb_ingress_controller" {
-  metadata {
-    name      = "aws-load-balancer-controller"
-    namespace = "kube-system"
-
-    annotations = {
-      "eks.amazonaws.com/role-arn" = aws_iam_role.alb_ingress_controller.arn
-    }
-  }
-}
