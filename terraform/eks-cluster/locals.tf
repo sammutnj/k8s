1e2c5416e810b617fcf5fc000723957dc602a272
sammutnj <sammutnj90@gmail.com>
2025-06-29 23:01:28 +1000

    d

--- terraform/eks-cluster/locals.tf
+++ terraform/eks-cluster/locals.tf
@@ -1,4 +1,4 @@
 locals {
-  certificate_arn = var.create_acm_certificate ? aws_acm_certificate.nginx[0].arn : var.acm_certificate_arn
-  region          = ap-southeast-2
+  certificate_arn = var.acm_certificate_arn
+  region          = "ap-southeast-2"
 }
