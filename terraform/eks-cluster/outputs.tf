df112a835656169611fc7102f3e2f3fd4b85ca98
sammutnj <sammutnj90@gmail.com>
2025-07-02 23:00:41 +1000

    d

--- terraform/eks-cluster/outputs.tf
+++ terraform/eks-cluster/outputs.tf
@@ -1,15 +1,15 @@
 output "cluster_name" {
-  value = module.eks.cluster_name
+  value = aws_eks_cluster.this.name
 }
 
 output "cluster_endpoint" {
-  value = module.eks.cluster_endpoint
+  value = aws_eks_cluster.this.endpoint
 }
 
 output "cluster_version" {
-  value = module.eks.cluster_version
+  value = aws_eks_cluster.this.version
 }
 
 output "oidc_provider_arn" {
-  value = module.eks.oidc_provider_arn
+  value = aws_eks_cluster.this.identity[0].oidc[0].issuer
 }
