12de1a0b3d9345a21be3d9136d4b3281545c6a0f
sammutnj <sammutnj90@gmail.com>
2025-07-06 23:32:31 +1000

    d

--- terraform/eks-cluster/main.tf
+++ terraform/eks-cluster/main.tf
@@ -34,11 +34,6 @@ provider "kubernetes" {
 }
 
 provider "helm" {
-  kubernetes {
-    host                   = aws_eks_cluster.this.endpoint
-    cluster_ca_certificate = base64decode(aws_eks_cluster.this.certificate_authority[0].data)
-    token                  = data.aws_eks_cluster_auth.this.token
-  }
 }
 
 resource "aws_eks_cluster" "this" {
