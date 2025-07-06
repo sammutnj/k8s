552a263c157d50b3404e3a14d381ce8155393df5
sammutnj <sammutnj90@gmail.com>
2025-07-06 22:18:45 +1000

    s

--- terraform/eks-cluster/main.tf
+++ terraform/eks-cluster/main.tf
@@ -26,7 +26,6 @@ provider "kubernetes" {
   host                   = aws_eks_cluster.this.endpoint
   cluster_ca_certificate = base64decode(aws_eks_cluster.this.certificate_authority[0].data)
   token                  = data.aws_eks_cluster_auth.this.token
-  load_config_file       = false
 }
 
 # EKS cluster auth data source for token
